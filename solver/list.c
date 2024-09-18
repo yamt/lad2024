2770205e7f1a3acee1e27dc4482632b4352bda6a
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-09-18 19:00:39 +0900

    add a simple solver
    list.[ch] and platform.h are taken from toywasm

---
+++ solver/list.c
@@ -0,0 +1,103 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "list.h"
+
+static void
+list_check(const struct list_head *h)
+{
+        assert(*h->tailnextp == NULL);
+        assert((h->tailnextp == &h->first) == (h->first == NULL));
+}
+
+static void
+list_check2(const struct list_head *h, const void *elem0,
+            const struct list_entry *e0, bool elem0_on_list)
+{
+#if !defined(NDEBUG)
+        bool on_list = false;
+        list_check(h);
+        uintptr_t offset = (uintptr_t)e0 - (uintptr_t)elem0;
+        const void *elem;
+        elem = h->first;
+        while (elem != NULL) {
+                if (elem == elem0) {
+                        on_list = true;
+                }
+                const struct list_entry *e =
+                        (void *)((uintptr_t)elem + offset);
+                assert(*e->prevnextp == elem);
+                const void *nextelem = e->next;
+                assert(nextelem != elem);
+                if (nextelem == NULL) {
+                        assert(h->tailnextp == &e->next);
+                }
+                elem = nextelem;
+        }
+        assert(on_list == elem0_on_list);
+#endif
+}
+
+void
+list_remove(struct list_head *h, void *elem, struct list_entry *e)
+{
+        list_check2(h, elem, e, true);
+        assert(h->first != NULL);
+        *e->prevnextp = e->next;
+        if (e->next == NULL) {
+                assert(h->tailnextp == &e->next);
+                h->tailnextp = e->prevnextp;
+        } else {
+                uintptr_t offset = (uintptr_t)e - (uintptr_t)elem;
+                struct list_entry *nextentry =
+                        (void *)((uintptr_t)e->next + offset);
+                assert(nextentry->prevnextp == &e->next);
+                nextentry->prevnextp = e->prevnextp;
+        }
+        list_check2(h, elem, e, false);
+}
+
+void
+list_insert_tail(struct list_head *h, void *elem, struct list_entry *e)
+{
+        list_check2(h, elem, e, false);
+        e->prevnextp = h->tailnextp;
+        *h->tailnextp = elem;
+        h->tailnextp = &e->next;
+        e->next = NULL;
+        assert(h->first != NULL);
+        assert(*e->prevnextp == elem);
+        list_check2(h, elem, e, true);
+}
+
+void
+list_insert_head(struct list_head *h, void *elem, struct list_entry *e)
+{
+        list_check2(h, elem, e, false);
+        if (h->first == NULL) {
+                e->next = NULL;
+                h->tailnextp = &e->next;
+        } else {
+                uintptr_t offset = (uintptr_t)e - (uintptr_t)elem;
+                struct list_entry *firstentry =
+                        (void *)((uintptr_t)h->first + offset);
+                assert(firstentry->prevnextp == &h->first);
+                firstentry->prevnextp = &e->next;
+                e->next = h->first;
+                e->prevnextp = &h->first;
+        }
+        h->first = elem;
+        assert(h->first != NULL);
+        assert(*e->prevnextp == elem);
+        list_check2(h, elem, e, true);
+}
+
+void
+list_head_init(struct list_head *h)
+{
+        h->first = NULL;
+        h->tailnextp = &h->first;
+        list_check(h);
+}
---
+++ solver/main.c
@@ -0,0 +1,157 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "defs.h"
+#include "hash.h"
+#include "list.h"
+#include "loader.h"
+#include "rule.h"
+
+#define HASH_SIZE 1024021
+
+struct node {
+        map_t map;
+        LIST_ENTRY(struct node) q;
+        LIST_ENTRY(struct node) hash;
+        struct node *parent;
+        unsigned int steps;
+
+        /* move from the parent. note: this is redundant. */
+        loc_t loc;
+        enum diridx dir;
+};
+
+LIST_HEAD_NAMED(struct node, hash_head) hash_heads[HASH_SIZE];
+LIST_HEAD(struct node) todo;
+
+struct node *
+alloc_node(void)
+{
+        struct node *n = malloc(sizeof(*n));
+        if (n == NULL) {
+                exit(1);
+        }
+        return n;
+}
+
+bool
+add(struct node *n)
+{
+        uint32_t hash = sdbm_hash(n->map, sizeof(n->map));
+        uint32_t idx = hash % HASH_SIZE;
+        struct hash_head *head = &hash_heads[idx];
+        struct node *n2;
+        LIST_FOREACH(n2, head, hash)
+        {
+                if (!memcmp(n2->map, n->map, sizeof(n->map))) {
+                        return true;
+                }
+        }
+        LIST_INSERT_TAIL(head, n, hash);
+        return false;
+}
+
+void
+dump_hash(void)
+{
+#define buckets 10
+        unsigned int count[buckets];
+        memset(count, 0, sizeof(count));
+        unsigned int i;
+        for (i = 0; i < HASH_SIZE; i++) {
+                unsigned int n = 0;
+                struct node *dn;
+                LIST_FOREACH(dn, &hash_heads[i], hash) { n++; }
+                if (n >= buckets - 1) {
+                        count[buckets - 1]++;
+                } else {
+                        count[n]++;
+                }
+        }
+        printf("hash stat\n");
+        for (i = 0; i < buckets; i++) {
+                printf("%4d %u\n", i, count[i]);
+        }
+}
+
+void
+dump(const struct node *n)
+{
+        do {
+                printf("step %3u: x=%u y=%u dir=%c\n", n->steps, loc_x(n->loc),
+                       loc_y(n->loc), "LDRU"[n->dir]);
+                n = n->parent;
+        } while (n->parent != NULL);
+}
+
+int
+main(int argc, char **argv)
+{
+        LIST_HEAD_INIT(&todo);
+        unsigned int i;
+        for (i = 0; i < HASH_SIZE; i++) {
+                LIST_HEAD_INIT(&hash_heads[i]);
+        }
+        unsigned int queued = 0;
+        unsigned int processed = 0;
+        unsigned int duplicated = 0;
+        struct node *n = alloc_node();
+        struct map_info info;
+        decode_stage(16, n->map, &info);
+        LIST_INSERT_TAIL(&todo, n, q);
+        n->parent = NULL;
+        n->steps = 0;
+        queued++;
+        add(n);
+        while ((n = LIST_FIRST(&todo)) != NULL) {
+                LIST_REMOVE(&todo, n, q);
+                struct stage_meta meta;
+                map_t beam_map;
+                calc_stage_meta(n->map, &meta);
+                calc_beam(n->map, beam_map);
+                for (i = 0; i < meta.nplayers; i++) {
+                        struct player *p = &meta.players[i];
+                        enum diridx dir;
+                        for (dir = 0; dir < 4; dir++) {
+                                unsigned int flags =
+                                        player_move(&meta, p, dir, n->map,
+                                                    beam_map, false);
+                                if ((flags & MOVE_OK) == 0) {
+                                        continue;
+                                }
+                                struct node *n2 = alloc_node();
+                                memcpy(n2->map, n->map, sizeof(n->map));
+                                struct stage_meta meta2 = meta;
+                                player_move(&meta2, &meta2.players[i], dir,
+                                            n2->map, beam_map, true);
+                                if (add(n2)) {
+                                        duplicated++;
+                                        free(n2);
+                                        continue;
+                                }
+                                n2->parent = n;
+                                n2->steps = n->steps + 1;
+                                n2->loc = p->loc;
+                                n2->dir = dir;
+                                LIST_INSERT_TAIL(&todo, n2, q);
+                                queued++;
+                                if (meta2.nbombs == 0) {
+                                        printf("solved!\n");
+                                        dump(n2);
+                                        dump_hash();
+                                        exit(0);
+                                }
+                        }
+                }
+                processed++;
+                if ((processed % 10000) == 0) {
+                        printf("processed %u / %u duplicated %u step %u\n",
+                               processed, queued, duplicated, n->steps);
+                        dump_hash();
+                }
+        }
+        printf("impossible\n");
+}
