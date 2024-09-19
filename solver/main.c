b26169bea919c661f80e5972e71c7f1490ca463a
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-09-20 00:39:02 +0900

    add a mode to evaluate all stages

--- solver/main.c
+++ solver/main.c
@@ -11,17 +11,9 @@
 #include "node.h"
 #include "solver.h"
 
-int
-main(int argc, char **argv)
+unsigned int
+load_and_evaluate_stage(unsigned int stage_number, struct evaluation *ev)
 {
-        if (argc != 2) {
-                exit(2);
-        }
-        int stage_number = atoi(argv[1]);
-        if (stage_number <= 0) {
-                exit(2);
-        }
-
         struct node *n = alloc_node();
         struct map_info info;
         printf("stage %u\n", stage_number);
@@ -36,9 +28,46 @@ main(int argc, char **argv)
         struct node_list solution;
         unsigned int result = solve(n, 10000000, &solution);
         if (result == SOLVE_SOLVED) {
-                struct evaluation ev;
-                evaluate(&solution, &ev);
-                printf("score %u\n", ev.score);
+                evaluate(&solution, ev);
         }
         solve_cleanup();
+        return result;
+}
+
+int
+main(int argc, char **argv)
+{
+        if (argc != 2) {
+                exit(2);
+        }
+        int stage_number = atoi(argv[1]);
+        if (stage_number < 0) {
+                exit(2);
+        }
+
+        if (stage_number > 0) {
+                struct evaluation ev;
+                load_and_evaluate_stage(stage_number, &ev);
+        } else {
+                struct result {
+                        unsigned int solve_flags;
+                        struct evaluation ev;
+                } *results;
+                results = calloc(sizeof(results), nstages);
+                if (results == NULL) {
+                        exit(1);
+                }
+                unsigned int i;
+                for (i = 0; i < nstages; i++) {
+                        struct result *r = &results[i];
+                        r->solve_flags =
+                                load_and_evaluate_stage(i + 1, &r->ev);
+                }
+                for (i = 0; i < nstages; i++) {
+                        struct result *r = &results[i];
+                        printf("stage %u solve_flags %u score %u\n", i + 1,
+                               r->solve_flags, r->ev.score);
+                }
+                free(results);
+        }
 }
