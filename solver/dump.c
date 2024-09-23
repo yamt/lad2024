98bf07b738d5fe2123e90d5d86c16ba0838d80a5
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-09-23 22:44:19 +0900

    add dump_map_c

--- solver/dump.c
+++ solver/dump.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "defs.h"
 #include "dump.h"
 
 char
@@ -28,4 +30,45 @@ dump_map(const map_t map)
                 }
                 printf("\n");
         }
+}
+
+void
+dump_map_c(const map_t map, const char *filename)
+{
+        FILE *fp = fopen(filename, "w");
+        if (fp == NULL) {
+                fprintf(stderr, "failed to open %s\n", filename);
+                exit(1);
+        }
+        int x;
+        int y;
+        fprintf(fp, "\t{\n");
+        fprintf(fp, "\t\t.data = (const uint8_t[]){\n");
+        for (y = 0; y < map_height; y++) {
+                const char *sep = "\t\t\t";
+                int line_size = map_width;
+                for (x = map_width - 1; x >= 0; x--) {
+                        loc_t loc = genloc(x, y);
+                        uint8_t objidx = map[loc];
+                        if (objidx != _) {
+                                break;
+                        }
+                        line_size = x;
+                }
+                if (line_size == 0) {
+                        break;
+                }
+                for (x = 0; x < line_size; x++) {
+                        loc_t loc = genloc(x, y);
+                        uint8_t objidx = map[loc];
+                        int chr = objchr(objidx);
+                        fprintf(fp, "%s%c,", sep, chr);
+                        sep = "";
+                }
+                fprintf(fp, " END,\n");
+        }
+        fprintf(fp, "\t\t\tEND, END,\n");
+        fprintf(fp, "\t\t},\n");
+        fprintf(fp, "\t},\n");
+        fclose(fp);
 }
--- solver/dump.h
+++ solver/dump.h
@@ -2,3 +2,4 @@
 
 char objchr(uint8_t objidx);
 void dump_map(const map_t map);
+void dump_map_c(const map_t map, const char *filename);
