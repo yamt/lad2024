a7e3ac0d11d822ed3cb981e195928437a28eba52
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-09-27 23:15:06 +0900

    bump the number of rooms

--- solver/generate.c
+++ solver/generate.c
@@ -102,7 +102,7 @@ generate(struct genctx *ctx)
         struct rng *rng = ctx->rng;
         int i;
         int n;
-        n = rng_rand(rng, 1, 6);
+        n = rng_rand(rng, 1, 8);
         for (i = 0; i < n; i++) {
                 room(ctx);
         }
