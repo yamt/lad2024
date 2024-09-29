d35d902d30afe03d93a852e9a3eb04472b9a166c
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-09-29 18:47:30 +0900

    make solver use the same limit as generater

--- solver/main.c
+++ solver/main.c
@@ -26,7 +26,8 @@ load_and_evaluate_stage(unsigned int stage_number, struct evaluation *ev)
         dump_map(n->map);
 
         struct solution solution;
-        size_t limit = (size_t)4 * 1024 * 1024 * 1024; /* 4GB */
+        // size_t limit = (size_t)4 * 1024 * 1024 * 1024; /* 4GB */
+        size_t limit = (size_t)8 * 1024 * 1024 * 1024; /* 8GB */
         unsigned int result = solve(n, limit, true, &solution);
         if (result == SOLVE_SOLVED) {
                 evaluate(n, &solution.moves, ev);
