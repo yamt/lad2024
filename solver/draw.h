724d5c5cb6c570a4fbe3b8dbc3191edc993508b5
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-09-30 23:46:11 +0900

    clang-format

--- solver/draw.h
+++ solver/draw.h
@@ -12,7 +12,7 @@ bool simple_put(uint8_t *p, void *arg);
 bool replace_obj(uint8_t *p, void *arg);
 
 bool random_place_obj_in_bb(struct rng *rng, map_t map, const struct bb *bb,
-                       putter_t putter, void *arg);
+                            putter_t putter, void *arg);
 bool random_place_objs_in_bb(struct rng *rng, map_t map, const struct bb *bb);
 
 void rect(map_t map, int rx, int ry, int rw, int rh, uint8_t objidx);
