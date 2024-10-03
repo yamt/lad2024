d6f1e6d853d5cb765f8121f79c1ea745b75f298c
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-10-03 20:41:06 +0900

    dirchr

--- solver/dump.h
+++ solver/dump.h
@@ -1,6 +1,7 @@
 #include "rule.h"
 
 char objchr(uint8_t objidx);
+char dirchr(enum diridx dir);
 void dump_map(const map_t map);
 void dump_map_raw(const map_t map);
 void dump_map_c(const map_t map, const char *filename);
