a9a7d8e7215528520cbc744fb0aead771d278791
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-10-07 19:35:41 +0900

    use the pool allocator for nodes
    this would speed up solver_cleanup

--- solver/node.c
+++ solver/node.c
@@ -1,15 +1,36 @@
 #include <stdlib.h>
 
 #include "node.h"
+#include "pool.h"
+
+struct pool nodepool;
+
+void
+node_allocator_init(void)
+{
+        pool_init(&nodepool, sizeof(struct node));
+}
 
 struct node *
 alloc_node(void)
 {
-        struct node *n = malloc(sizeof(*n));
+        struct node *n = pool_item_alloc(&nodepool);
         if (n == NULL) {
                 exit(1);
         }
         return n;
+}
+
+void
+free_node(struct node *n)
+{
+        pool_item_free(&nodepool, n);
+}
+
+void
+free_all_nodes(void)
+{
+        pool_all_items_free(&nodepool);
 }
 
 loc_t
