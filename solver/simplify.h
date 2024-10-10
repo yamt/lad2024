d8582670f5de866478ea34737f56a8b101659589
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-10-10 23:36:14 +0900

    remove redundant function prototypes

--- solver/simplify.h
+++ solver/simplify.h
@@ -2,7 +2,3 @@
 
 void simplify(map_t map);
 void align_to_top_left(map_t map);
-
-bool calc_reachable_from_A(const map_t map, const map_t movable,
-                           map_t reachable);
-void calc_movable(const map_t map, map_t movable);
