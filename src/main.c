2074e35373e6b0ac0d5b4f56d06dde91d4889d35
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-10-10 22:39:56 +0900

    make ASSERET verbose for debug build

--- src/main.c
+++ src/main.c
@@ -8,12 +8,23 @@
 #include "rule.h"
 #include "wasm4.h"
 
+#if !defined(NDEBUG)
+#define ASSERT(cond)                                                          \
+        do {                                                                  \
+                if (!(cond)) {                                                \
+                        tracef("assertion (%s) failed at %s:%d", #cond,       \
+                               __FILE__, __LINE__);                           \
+                        __builtin_trap();                                     \
+                }                                                             \
+        } while (0)
+#else
 #define ASSERT(cond)                                                          \
         do {                                                                  \
                 if (!(cond)) {                                                \
                         __builtin_trap();                                     \
                 }                                                             \
         } while (0)
+#endif
 
 enum sprite_idx {
         SPIDX_NONE = -1,
