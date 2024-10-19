86f045755aedc6b18c9ef90fc412aead85ed2077
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-10-19 11:15:27 +0900

    disable eager refinement for now

--- tools/refine.h
+++ tools/refine.h
@@ -5,6 +5,6 @@
 struct solution;
 struct solver_param;
 
-bool refine(map_t map, const struct solution *solution);
+bool refine(map_t map, bool eager, const struct solution *solution);
 bool try_refine(map_t map, struct solution *solution,
                 const struct solver_param *param);
