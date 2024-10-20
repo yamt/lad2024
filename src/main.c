1b3b485aa1d0fcda4296940f3e7c9303b065f11b
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-10-20 13:05:57 +0900

    bump max_stages

--- src/main.c
+++ src/main.c
@@ -289,7 +289,7 @@ static struct stage_draw_info {
         const char *message;
 } draw_info;
 
-#define max_stages 400
+#define max_stages 500
 #define save_data_version 1
 struct save_data {
         uint32_t version;
