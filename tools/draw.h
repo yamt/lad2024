3045e536d7a8d2c5154ffc1d2456e454bab38190
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-10-30 20:35:06 +0900

    random_place_objs_in_bb_with_freq

--- tools/draw.h
+++ tools/draw.h
@@ -15,6 +15,16 @@ bool random_place_obj_in_bb(struct rng *rng, map_t map, const struct bb *bb,
                             putter_t putter, void *arg);
 bool random_place_objs_in_bb(struct rng *rng, map_t map, const struct bb *bb);
 
+struct freq {
+        uint8_t objidx;
+        unsigned int freq;
+};
+
+bool random_place_objs_in_bb_with_freq(struct rng *rng, map_t map,
+                                       const struct bb *bb,
+                                       const struct freq *freq,
+                                       unsigned int nfreq);
+
 void rect(map_t map, int rx, int ry, int rw, int rh, uint8_t objidx);
 void box(map_t map, int rx, int ry, int rw, int rh, uint8_t objidx);
 void circle(map_t map, int x, int y, int r, uint8_t objidx);
