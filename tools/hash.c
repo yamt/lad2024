5e307ec2b380003b0ef6edd269e33a409005d65e
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-11-02 16:59:41 +0900

    fletcher32
    for some reasons this performs horribly when we have a lot of nodes.
    (compared to the sdbm hash we are currently using)
    maybe because of hash collisions?

--- tools/hash.c
+++ tools/hash.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "hash.h"
 
 uint32_t
@@ -10,4 +12,38 @@ sdbm_hash(const void *p, size_t len)
                 hash = hash * 65599 + cp[i];
         }
         return hash;
+}
+
+uint32_t
+fletcher32(const void *p, size_t len)
+{
+        const uint8_t *cp = p;
+        uint32_t a;
+        uint32_t b;
+
+        a = b = 0;
+        while (len > 1) {
+                size_t blen = len & ~1;
+                if (blen > 360 * 2) {
+                        blen = 360 * 2;
+                }
+                len -= blen;
+                do {
+                        uint16_t v;
+                        memcpy(&v, cp, sizeof(v));
+                        a += v;
+                        b += a;
+                        cp += sizeof(v);
+                        blen -= sizeof(v);
+                } while (blen > 0);
+                a %= 65535;
+                b %= 65535;
+        }
+        if (len > 0) {
+                a += *cp;
+                b += a;
+                a %= 65535;
+                b %= 65535;
+        }
+        return (b << 16) | a;
 }
--- tools/hash.h
+++ tools/hash.h
@@ -2,3 +2,4 @@
 #include <stdint.h>
 
 uint32_t sdbm_hash(const void *p, size_t len);
+uint32_t fletcher32(const void *p, size_t len);
