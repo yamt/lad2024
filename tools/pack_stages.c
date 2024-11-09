8d8dc4bc9d7e1461027c5cb6cd5bd3eab37c086b
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-11-09 21:40:09 +0900

    pack_stages: dump C code

--- tools/pack_stages.c
+++ tools/pack_stages.c
@@ -32,6 +32,9 @@ stage_data_size(const uint8_t *p)
 int
 main(int argc, char **argv)
 {
+        size_t curoff = 0;
+        size_t offset[nstages];
+
         struct hufftree huff;
         huff_init(&huff);
         unsigned int i;
@@ -47,15 +50,18 @@ main(int argc, char **argv)
         size_t htablesize;
         huff_table(&huff, htable, &htablesize);
 
+        printf("const uint8_t stages_huff_data[] = {\n");
         for (i = 0; i < nstages; i++) {
+                offset[i] = curoff;
                 const struct stage *stage = &stages[i];
                 const uint8_t *data = stage->data;
                 size_t data_size = stage_data_size(data);
                 uint8_t encoded[data_size];
                 size_t encoded_len;
                 huff_encode(&huff, data, data_size, encoded, &encoded_len);
-                printf("stage %03u %zu bytes (%.1f %%)\n", i + 1, encoded_len,
-                       (float)encoded_len / data_size * 100);
+                printf("// stage %03u %zu bytes (%.1f %%)\n", i + 1,
+                       encoded_len, (float)encoded_len / data_size * 100);
+                curoff += encoded_len;
 
                 struct huff_decode_context ctx;
                 huff_decode_init(&ctx, encoded);
@@ -65,6 +71,40 @@ main(int argc, char **argv)
                         decoded[j] = huff_decode_byte(&ctx, htable);
                 }
                 assert(!memcmp(data, decoded, data_size));
+                for (j = 0; j < encoded_len; j++) {
+                        printf("%#02x,", (unsigned int)encoded[j]);
+                }
+                printf("\n");
         }
+        printf("};\n");
+
+        printf("const struct stage packed_stages[] = {\n");
+        for (i = 0; i < nstages; i++) {
+                const struct stage *stage = &stages[i];
+
+                printf("\t[%u] = {\n", i + 1);
+                printf("\t\t.data_offset = %zu,\n", offset[i]);
+                if (stage->message != NULL) {
+                        printf("\t\t.message = (const uint8_t[]){\n");
+
+                        const char *cp = stage->message;
+                        char ch;
+                        do {
+                                ch = *cp++;
+                                printf("%#02x,", (unsigned int)(uint8_t)ch);
+                        } while (ch != 0);
+                        printf("\n");
+
+                        printf("\t\t},\n");
+                }
+                printf("\t},\n");
+        }
+        printf("};\n");
+
+        printf("const uint8_t stage_huff_table[] = {\n");
+        for (i = 0; i < htablesize; i++) {
+                printf("%02x,", (unsigned int)htable[i]);
+        }
+        printf("};\n");
         exit(0);
 }
