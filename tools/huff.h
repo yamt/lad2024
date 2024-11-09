0e586d4bca07657f323a37184188a8cac90adec1
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-11-09 20:48:41 +0900

    format

--- tools/huff.h
+++ tools/huff.h
@@ -3,30 +3,31 @@
 #include <stdint.h>
 
 struct hnode {
-	bool is_leaf;
-    size_t count;
-    union {
-        struct leaf {
-            uint8_t value;
-            uint8_t encoded_nbits;
-            uint16_t encoded_bits;
-        } leaf;
-        struct inner {
-            struct hnode *children[2];
-            uint8_t idx;
-		} inner;
-    } u;
+        bool is_leaf;
+        size_t count;
+        union {
+                struct leaf {
+                        uint8_t value;
+                        uint8_t encoded_nbits;
+                        uint16_t encoded_bits;
+                } leaf;
+                struct inner {
+                        struct hnode *children[2];
+                        uint8_t idx;
+                } inner;
+        } u;
 };
 
 struct hufftree {
-	struct hnode nodes[256 * 2 - 1];
+        struct hnode nodes[256 * 2 - 1];
 };
 
 void huff_init(struct hufftree *tree);
 void huff_update(struct hufftree *tree, const uint8_t *p, size_t len);
 void huff_build(struct hufftree *tree);
 
-void huff_encode(const struct hufftree *tree, const uint8_t *p, size_t len, uint8_t *out, size_t *lenp);
+void huff_encode(const struct hufftree *tree, const uint8_t *p, size_t len,
+                 uint8_t *out, size_t *lenp);
 
 #define HUFF_TABLE_SIZE_MAX (255 * 3)
 void huff_table(const struct hufftree *tree, uint8_t *out, size_t *lenp);
--- tools/huff_decode.c
+++ tools/huff_decode.c
@@ -6,37 +6,37 @@
 void
 huff_decode_init(struct huff_decode_context *ctx, const uint8_t *p)
 {
-	ctx->p = p;
-    ctx->bitoff = 0;
+        ctx->p = p;
+        ctx->bitoff = 0;
 }
 
 static uint8_t
 huff_get_raw_bit(struct huff_decode_context *ctx)
 {
-	uint8_t u8 = *ctx->p;
-    uint8_t bit = (u8 >> (7 - ctx->bitoff)) & 1;
-    ctx->bitoff++;
-    if (ctx->bitoff == 8) {
-        ctx->bitoff = 0;
-        ctx->p++;
-    }
-    return bit;
+        uint8_t u8 = *ctx->p;
+        uint8_t bit = (u8 >> (7 - ctx->bitoff)) & 1;
+        ctx->bitoff++;
+        if (ctx->bitoff == 8) {
+                ctx->bitoff = 0;
+                ctx->p++;
+        }
+        return bit;
 }
 
 uint8_t
 huff_decode_byte(struct huff_decode_context *ctx, const uint8_t *table)
 {
-	const uint8_t *entry = table;
-    while (true) {
-        uint8_t bit = huff_get_raw_bit(ctx);
-        assert(bit == 0 || bit == 1);
-        // printf("decode bit %u\n", bit);
-        if (((*entry) & (1 << bit)) != 0) {
-            return entry[bit + 1];
-        } else {
-            uint8_t idx = entry[bit + 1];
-            assert(idx != 0);
-            entry = &table[3 * idx];
+        const uint8_t *entry = table;
+        while (true) {
+                uint8_t bit = huff_get_raw_bit(ctx);
+                assert(bit == 0 || bit == 1);
+                // printf("decode bit %u\n", bit);
+                if (((*entry) & (1 << bit)) != 0) {
+                        return entry[bit + 1];
+                } else {
+                        uint8_t idx = entry[bit + 1];
+                        assert(idx != 0);
+                        entry = &table[3 * idx];
+                }
         }
-	}
 }
