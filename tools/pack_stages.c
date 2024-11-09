4699333fe99f7bb857388e5d4622d54e966a9cc8
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-11-09 21:11:09 +0900

    pack_stages.c: operate on the encoded stages instead of decoded one

--- tools/pack_stages.c
+++ tools/pack_stages.c
@@ -11,6 +11,23 @@
 #include "huff_decode.h"
 #include "loader.h"
 #include "maputil.h"
+#include "stages.h"
+
+size_t
+stage_data_size(const uint8_t *p)
+{
+        const uint8_t *sp = p;
+        uint8_t ch;
+        while (true) {
+                ch = *p++;
+                if (ch == END) {
+                        ch = *p++;
+                        if (ch == END) {
+                                return p - sp;
+                        }
+                }
+        }
+}
 
 int
 main(int argc, char **argv)
@@ -19,10 +36,10 @@ main(int argc, char **argv)
         huff_init(&huff);
         unsigned int i;
         for (i = 0; i < nstages; i++) {
-                map_t map;
-                struct map_info info;
-                decode_stage(i, map, &info);
-                huff_update(&huff, map, map_size);
+                const struct stage *stage = &stages[i];
+                const uint8_t *data = stage->data;
+                size_t data_size = stage_data_size(data);
+                huff_update(&huff, data, data_size);
         }
         huff_build(&huff);
 
@@ -31,23 +48,23 @@ main(int argc, char **argv)
         huff_table(&huff, htable, &htablesize);
 
         for (i = 0; i < nstages; i++) {
-                map_t map;
-                struct map_info info;
-                decode_stage(i, map, &info);
-                uint8_t encoded[map_size];
+                const struct stage *stage = &stages[i];
+                const uint8_t *data = stage->data;
+                size_t data_size = stage_data_size(data);
+                uint8_t encoded[data_size];
                 size_t encoded_len;
-                huff_encode(&huff, map, map_size, encoded, &encoded_len);
+                huff_encode(&huff, data, data_size, encoded, &encoded_len);
                 printf("stage %03u %zu bytes (%.1f %%)\n", i + 1, encoded_len,
-                       (float)encoded_len / map_size * 100);
+                       (float)encoded_len / data_size * 100);
 
                 struct huff_decode_context ctx;
                 huff_decode_init(&ctx, encoded);
-                map_t map2;
-                loc_t loc;
-                for (loc = 0; loc < map_size; loc++) {
-                        map2[loc] = huff_decode_byte(&ctx, htable);
+                uint8_t decoded[data_size];
+                unsigned int j;
+                for (j = 0; j < data_size; j++) {
+                        decoded[j] = huff_decode_byte(&ctx, htable);
                 }
-                assert(!memcmp(map, map2, map_size));
+                assert(!memcmp(data, decoded, data_size));
         }
         exit(0);
 }
