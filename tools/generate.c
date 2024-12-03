95676e3e6366a8b94f27ea742baa056d76be6f76
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-12-03 22:12:34 +0900

    tweak parameters

--- tools/generate.c
+++ tools/generate.c
@@ -30,7 +30,7 @@ struct genctx {
 bool
 room(struct genctx *ctx, bool avoid_overlap, bool connect)
 {
-        const int max = 10;
+        const int max = 8;
         const int min = 1;
         const struct bb *bb = &ctx->bb;
         int rw = rng_rand(ctx->rng, min, max);
@@ -204,7 +204,7 @@ generate(struct genctx *ctx)
                 }
                 count_objects(ctx->map, count);
                 connect = true;
-        } while (100 * count[_] / map_size < 5);
+        } while (100 * count[_] / map_size < 8);
 #endif
 
 #if 0
