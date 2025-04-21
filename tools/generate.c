03142c73c7dae97a72cdfce17690a7a2cef8aed6
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-04-21 22:15:27 +0900

    tweak parameters

--- tools/generate.c
+++ tools/generate.c
@@ -105,6 +105,13 @@ generate(struct genctx *ctx)
 {
         rect(ctx->map, 0, 0, map_width, map_height, W);
 
+#if 1
+        rect(ctx->map, 1, 1, 4, 6, _);
+#endif
+#if 0
+        rect(ctx->map, 1, 1, 5, 5, _);
+#endif
+
 #if 0
         rect(ctx->map, 1, 1, 3, 3, _);
         rect(ctx->map, 7, 1, 3, 3, _);
@@ -197,7 +204,7 @@ generate(struct genctx *ctx)
         }
 #endif
 
-#if 1
+#if 0
         unsigned int count[END];
         bool connect = false;
         do {
