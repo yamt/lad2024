6e255e8fe654f837b2680a25340e524a251ff9f3
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-08-30 12:24:29 +0900

    comment

--- tools/info.h
+++ tools/info.h
@@ -1,6 +1,18 @@
 #include <stdbool.h>
 
+/*
+ * initialization
+ */
+void siginfo_setup_handler(void);
+
+/*
+ * set/clear the message for SIGINFO
+ */
 void siginfo_set_message(const char *msg, ...);
 void siginfo_clear_message(void);
-void siginfo_setup_handler(void);
+
+/*
+ * an alternative method for performance sensitive places,
+ * where frequent siginfo_set_message is not desirable.
+ */
 bool siginfo_latch_pending(void);
