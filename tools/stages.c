dfeca2199fc1497fcd8dbcdf2d01bddd4263f45b
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-10-05 18:57:00 +0900

    wip stage

--- tools/stages.c
+++ tools/stages.c
@@ -287,6 +287,29 @@ const struct stage stages[] = {
     },
 
 #include "generated_stages.inc"
+
+#if 0
+    {
+        .data = (const uint8_t[]){
+            W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, END,
+            W, _, L, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, END,
+            W, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, W, END,
+            W, _, X, _, X, _, X, _, X, _, X, _, X, _, _, _, _, _, W, END,
+            W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, _, W, W, END,
+            W, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, W, END,
+            W, _, _, _, X, _, X, _, X, _, X, _, X, _, X, _, X, _, _, W, END,
+            W, W, W, _, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, END,
+            W, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, W, END,
+            W, _, X, _, X, _, X, _, X, _, X, _, X, _, _, _, _, _, W, END,
+            W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, _, W, W, END,
+            W, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, W, END,
+            W, _, P, _, A, _, X, _, X, _, X, _, X, _, X, _, X, _, W, END,
+            W, _, _, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, END,
+            W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, END,
+            END,
+        },
+    },
+#endif
 };
 
 /* clang-format on */
