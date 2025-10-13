07a9195cf1ee4339f1aa7130c3d9800ed6c26832
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-10-13 22:51:07 +0900

    add tiles for tiled
    cf. https://www.mapeditor.org/

---
+++ tools/edit/lad-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="lad-tiles" tilewidth="8" tileheight="8" tilecount="9" columns="9">
+ <image source="lad-tiles.png" width="72" height="8"/>
+</tileset>
