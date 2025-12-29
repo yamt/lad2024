d35bb3f5974e2766708ff2e40c949b3203558264
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-12-29 17:51:42 +0900

    fix ASSERT

--- src/util.h
+++ src/util.h
@@ -1,18 +1,11 @@
 #if defined(NDEBUG)
-
 #define CHECK(cond)                                                           \
         do {                                                                  \
                 if (!(cond)) {                                                \
                         __builtin_trap();                                     \
                 }                                                             \
         } while (0)
-
-#define ASSERT(cond)
-
 #else /* defined(NDEBUG) */
-
-#if defined(__wasm__)
-#include "wasm4.h"
 #define CHECK(cond)                                                           \
         do {                                                                  \
                 if (!(cond)) {                                                \
@@ -21,10 +14,16 @@
                         __builtin_trap();                                     \
                 }                                                             \
         } while (0)
-#define
+#endif /* defined(NDEBUG) */
+
+#if defined(NDEBUG)
+#define ASSERT(cond)
+#else /* defined(NDEBUG) */
+#if defined(__wasm__)
+#include "wasm4.h"
+#define ASSERT(cond) CHECK(cond)
 #else /* defined(__wasm__) */
 #include <assert.h>
 #define ASSERT(cond) assert(cond)
 #endif /* defined(__wasm__) */
-
 #endif /* defined(NDEBUG) */
