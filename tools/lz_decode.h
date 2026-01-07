35036761224b0ad9c20a9d18d4b4068a85158e25
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-01-07 23:17:06 +0900

    clang-format

--- tools/lz_decode.h
+++ tools/lz_decode.h
@@ -5,9 +5,9 @@
 #define LZ_DECODE_BUF_SIZE (MATCH_DISTANCE_MAX + MATCH_LEN_MAX)
 
 struct lz_decode_state {
-    woff_t readidx;
-    woff_t endidx;
-    uint8_t buf[LZ_DECODE_BUF_SIZE];
+        woff_t readidx;
+        woff_t endidx;
+        uint8_t buf[LZ_DECODE_BUF_SIZE];
 };
 
 void lz_decode_init(struct lz_decode_state *s);
