2762572a8246c8d702590d26ec7ca52dc9750d5c
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-01-07 23:13:30 +0900

    clang-format

--- tools/lzhuff_decode.h
+++ tools/lzhuff_decode.h
@@ -5,10 +5,7 @@
 struct lz_decode_state;
 struct bitin;
 
-void
-lzhuff_decode_init(struct lz_decode_state *s);
-huff_sym_t
-lzhuff_decode_sym(struct lz_decode_state *s, struct bitin *in, 
-                   const uint8_t *huff_lit,
-                   const uint8_t *huff_dist,
-                   huff_sym_t match_base);
+void lzhuff_decode_init(struct lz_decode_state *s);
+huff_sym_t lzhuff_decode_sym(struct lz_decode_state *s, struct bitin *in,
+                             const uint8_t *huff_lit, const uint8_t *huff_dist,
+                             huff_sym_t match_base);
