08154a8c1a185aee731b8e50e8675f4ae368b098
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-01-09 23:49:05 +0900

    test_util.c

--- tools/lzhuff_test.c
+++ tools/lzhuff_test.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "test_util.h"
+
 #include "bitbuf.h"
 #include "lzhuff.h"
 
@@ -15,36 +17,6 @@
 #include "lzhuff_decode.h"
 
 #include "huff_debug.h"
-
-static void *
-read_fd(int fd, size_t *szp)
-{
-        uint8_t *input = NULL;
-        size_t inputsize = 0;
-        size_t bufsz = 16;
-        while (1) {
-                input = realloc(input, bufsz);
-                if (input == NULL) {
-                        fprintf(stderr, "realloc failed\n");
-                        exit(1);
-                }
-                ssize_t nread = read(fd, &input[inputsize], bufsz - inputsize);
-                if (nread == 0) {
-                        break;
-                }
-                if (nread == -1) {
-                        fprintf(stderr, "read failed\n");
-                        exit(1);
-                }
-                inputsize += nread;
-                assert(inputsize <= bufsz);
-                if (inputsize == bufsz) {
-                        bufsz *= 2;
-                }
-        }
-        *szp = inputsize;
-        return input;
-}
 
 int
 main(void)
