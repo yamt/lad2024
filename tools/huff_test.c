9bedebff2a31c59d0515d95b12bdbb93f55c9d9d
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-01-09 23:51:04 +0900

    use test_util.c

--- tools/huff_test.c
+++ tools/huff_test.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "test_util.h"
+
 #include "bitin.h"
 #include "huff.h"
 #include "huff_debug.h"
@@ -15,32 +17,8 @@
 int
 main(void)
 {
-        uint8_t *input = NULL;
-        size_t inputsize = 0;
-        size_t bufsz = 16;
-        while (1) {
-                input = realloc(input, bufsz);
-                if (input == NULL) {
-                        fprintf(stderr, "realloc failed\n");
-                        exit(1);
-                }
-                ssize_t nread = read(STDIN_FILENO, &input[inputsize],
-                                     bufsz - inputsize);
-                if (nread == 0) {
-                        break;
-                }
-                if (nread == -1) {
-                        fprintf(stderr, "read failed\n");
-                        exit(1);
-                }
-                inputsize += nread;
-                assert(inputsize <= bufsz);
-                if (inputsize == bufsz) {
-                        bufsz *= 2;
-                }
-        }
-
-        printf("buffer size: %zu bytes\n", bufsz);
+        size_t inputsize;
+        uint8_t *input = read_fd(STDIN_FILENO, &inputsize);
         printf("input size: %zu bytes\n", inputsize);
 
         struct hufftree t;
