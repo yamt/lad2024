7735e4a05149135a41dfcf64eef616dd8c067ba4
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-01-14 23:26:13 +0900

    sync bitbuf.[ch] with garbage repo
    commit 0420a7a380ad303023b8e9c585d1c85205cdc71d

--- tools/bitbuf.c
+++ tools/bitbuf.c
@@ -42,15 +42,15 @@ bitbuf_flush1(struct bitbuf *s, unsigned int thresh)
         }
 }
 
-static void
-bitbuf_write1(struct bitbuf *s, uint16_t bits, uint8_t nbits)
+void
+bitbuf_write(struct bitbuf *s, uint16_t bits, uint8_t nbits)
 {
         /*
          * input: the least significant "nbits" of "bits".
          * output: s->buf is filled from MSBs.
          */
         assert(nbits > 0);
-        assert(nbits <= 8);
+        assert(nbits <= 16);
 
         /* assert that unused bits are zero */
         assert((bits & (0xffff << nbits)) == 0);
@@ -63,15 +63,15 @@ bitbuf_write1(struct bitbuf *s, uint16_t bits, uint8_t nbits)
 }
 
 void
-bitbuf_write(struct bitbuf *s, const uint8_t *bits, uint16_t nbits)
+bitbuf_write_multi(struct bitbuf *s, const uint8_t *bits, size_t nbits)
 {
         assert(bits != NULL);
         assert(nbits != 0);
         while (nbits > 8) {
-                bitbuf_write1(s, *bits++, 8);
+                bitbuf_write(s, *bits++, 8);
                 nbits -= 8;
         }
-        bitbuf_write1(s, *bits, nbits);
+        bitbuf_write(s, *bits, nbits);
 }
 
 void
@@ -85,4 +85,87 @@ bitbuf_clear(struct bitbuf *s)
 {
         free(s->p);
         bitbuf_init(s);
+}
+
+/* reversed output */
+
+static void
+bitbuf_rev_output_byte(struct bitbuf *s, uint8_t data)
+{
+        if (s->datalen == s->allocated) {
+                size_t newsize = s->allocated * 2;
+                if (newsize < 64) {
+                        newsize = 64;
+                }
+                uint8_t *np = realloc(s->p, newsize);
+                if (np == NULL) {
+                        abort(); /* XXX */
+                }
+                memmove(np + newsize - s->datalen, np, s->datalen);
+                s->p = np;
+                s->allocated = newsize;
+        }
+        s->p[s->allocated - (++s->datalen)] = (uint8_t)s->buf;
+}
+
+static void
+bitbuf_rev_flush1(struct bitbuf *s, unsigned int thresh)
+{
+        while (s->bufoff >= thresh) {
+                bitbuf_rev_output_byte(s, s->buf & 0xff);
+                s->buf >>= 8;
+                if (s->bufoff > 8) {
+                        s->datalen_bits += 8;
+                        s->bufoff -= 8;
+                } else {
+                        s->datalen_bits += s->bufoff;
+                        s->bufoff = 0;
+                }
+        }
+}
+
+void
+bitbuf_rev_write(struct bitbuf *s, uint16_t bits, uint8_t nbits)
+{
+        /*
+         * input: the least significant "nbits" of "bits".
+         * output: s->buf is filled from LSBs.
+         */
+        assert(nbits > 0);
+        assert(nbits <= 16);
+
+        /* assert that unused bits are zero */
+        assert((bits & (0xffff << nbits)) == 0);
+        assert((s->buf & (0xffffffff << s->bufoff)) == 0);
+
+        unsigned int shift = s->bufoff;
+        s->buf |= (uint32_t)bits << shift;
+        s->bufoff += nbits;
+        bitbuf_rev_flush1(s, 8);
+}
+
+void
+bitbuf_rev_flush(struct bitbuf *s)
+{
+        bitbuf_rev_flush1(s, 1);
+        assert((s->datalen_bits + 7) / 8 == s->datalen);
+        unsigned int shift = (-s->datalen_bits) % 8;
+
+        /* shift all data to make the first bit byte aligned */
+        const uint8_t *cp = &s->p[s->allocated - s->datalen];
+        uint8_t *dp = s->p;
+        assert(dp <= cp);
+        uint8_t next = *cp;
+        assert((next & (0xff << (8 - shift))) == 0);
+        size_t i;
+        for (i = 0; i < s->datalen; i++) {
+                uint8_t b = next;
+                if (i < s->datalen - 1) {
+                        next = cp[1];
+                } else {
+                        next = 0;
+                }
+                cp++;
+                *dp++ = (b << shift) | (next >> (8 - shift));
+        }
 }
--- tools/bitbuf.h
+++ tools/bitbuf.h
@@ -1,7 +1,7 @@
 /*
  * bit-stream output buffer
  *
- * the format of the bit stream: within a byte, the MSB is first.
+ * the format of the output bit stream: within a byte, MSB are first.
  */
 
 #include <stdbool.h>
@@ -17,10 +17,24 @@ struct bitbuf {
         uint8_t *p;
         size_t datalen;
         size_t allocated;
+        size_t datalen_bits;
 };
 
 void bitbuf_init(struct bitbuf *s);
-/* bitbuf_write: append the least significant "nbits" of "bits". */
-void bitbuf_write(struct bitbuf *s, const uint8_t *bits, uint16_t nbits);
+/*
+ * bitbuf_write: append the least significant "nbits" of "bits".
+ *
+ * bitbuf_write_multi: append multiple bytes.
+ * if the last byte for bitbuf_write_bytes is partial, it's written as
+ * it would be with bitbuf_write(s, last_byte, nbits % 8). that is,
+ * the LSBs of the last byte was written. note that it's different
+ * from the output bit stream, where the last partial byte is filled
+ * from MSBs.
+ */
+void bitbuf_write(struct bitbuf *s, uint16_t bits, uint8_t nbits);
+void bitbuf_write_multi(struct bitbuf *s, const uint8_t *bits, size_t nbits);
 void bitbuf_flush(struct bitbuf *s);
 void bitbuf_clear(struct bitbuf *s);
+
+void bitbuf_rev_write(struct bitbuf *s, uint16_t bits, uint8_t nbits);
+void bitbuf_rev_flush(struct bitbuf *s);
